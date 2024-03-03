83f23525c362474fdfc9b6d4f792033d0a6d755a
Lakshya Agarwal <lakshyachampion.com>
2024-03-03 18:17:38 +0530

    frontend component and services added

--- frontend/Chatterbox-UI/src/app/app.module.ts
+++ frontend/Chatterbox-UI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
